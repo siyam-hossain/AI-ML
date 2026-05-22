03b36ca77b253e2e7f29b83c0ffb3c5a7c40e8e4
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-05-22 18:20:41 +0600

    008-import-001-all

---
+++ 008-importing-creating-modules-and-packages/001-01-test.py
@@ -0,0 +1,4 @@
+from package.maths import *
+
+print(f"addition: {addition(123,45)}")
+print(f"subtraction: {subtraction(7895,5445)}")
---
+++ 008-importing-creating-modules-and-packages/001-02-test.py
@@ -0,0 +1,3 @@
+from package.subpackages.mul import multiply
+
+print(multiply(4,5))
